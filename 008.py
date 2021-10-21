4be5c7aaf224f92091f5806e2d2db7f574e410e1
Thaisa Kobayashi <thaisa.kobayashi@gmail.com>
2021-10-21 00:55:32 -0300

    Inicio da aula 008, ex 014 e 015 para fazer

--- 008.py
+++ 008.py
@@ -1,7 +1,25 @@
-'''Utilizando Módulos'''
+'''Utilizando Módulos - Biblioteca'''
+
+#bebida
+#comida
+#doce
+
+#from typing import Match
+#import(bebida)
+#from(doce(import))
+#math
+
+#import math
+#num = int(input('Digite um número:'))
+#raiz = math.sqrt(num)
+#print('A raíz de {} é igual a {:.2f}'.format(num, math.floor(raiz)))
 
 
+#import random
+# num = random.randint(1, 10)
 
+import emoji
+print(emoji.emojize("Olá, Mundo: earth_americans"))
 
 
 
